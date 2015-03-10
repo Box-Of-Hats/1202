2011b3e458c09610513e15c762856713aae561d6
vault-hunter <ja1ke@hotmail.co.uk>
2015-03-10 18:01:29 +0000

    Test commit

---
+++ Register_User_Details.py
@@ -0,0 +1,12 @@
+[- Register user details
+	{input:
+		-student number(int)
+		-course(string)
+		-year of study(int)
+		
+	output:
+		-save to a temporary list(string)
+		}
+
+
+def Register_User_Details():
