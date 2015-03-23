37cd2598942fabaf23937a17f9adb544c3e100a7
Jack Edwards <jackedwards@outlook.com>
2015-03-23 17:56:36 +0000

    Updated the DataHandler class, it is now more flexible.

--- datahandler.py
+++ datahandler.py
@@ -1,8 +1,16 @@
 import pickle
 import os
+
 """
-This is how the student class will have to be defined in another file to work
-with the DataHandler.
+HOW TO USE THIS CLASS:
+When a DataHandler is created it checks if a data.dat file exists, and
+if it does it loads the student data currently stored in it.
+
+Use the add_student function to add a new student to the file.
+The 'students' array holds all of the student data, so to access student
+data you would simply type something like print(students[0])
+
+Below is a template for the student class used for testing.
 
 class Student:
     def __init__(self,
@@ -21,36 +29,30 @@ class Student:
 
 class DataHandler:
     def __init__(self):
-        self.data_table = []
+        self.students = []
         if os.path.isfile('data.dat'):
             self.retrieve()
 
-    def add_data(self, student):
-        row = [student.student_id,
-               student.year_of_study,
-               student.course,
-               student.vark_answers,
-               student.hm_answers]
-
-        self.data_table.append(row)
+    def add_student(self, student):
+        self.students.append(student)
         self.store()
 
-    def print_data(self):
-        for row in self.data_table:
-            print("Student ID:", row[0])
-            print("Year of study:", row[1])
-            print("Course:", row[2])
-            print("VARK answers:", row[3])
-            print("Honey & Mumford answers:", row[4])
+    def print_students(self):
+        for student in self.students:
+            print("Student ID:", student.student_id)
+            print("Year of study:", student.year_of_study)
+            print("Course:", student.course)
+            print("VARK answers:", student.vark_answers)
+            print("Honey & Mumford answers:", student.hm_answers)
 
     def store(self):
         dataFile = open('data.dat', 'wb')
-        pickle.dump(self.data_table, dataFile)
+        pickle.dump(self.students, dataFile)
         dataFile.close()
 
     def retrieve(self):
         dataFile = open('data.dat', 'rb')
-        self.data_table = pickle.load(dataFile)
+        self.students = pickle.load(dataFile)
         dataFile.close()
 
 
